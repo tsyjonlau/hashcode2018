5d982684a0a1e42a29423c46e61740995a20a8f3
Tsy-Jon Lau <tsy-jon.lau@epitech.eu>
2018-03-01 19:18:28 +0100

    update parser

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import parser
 # File name input
 fileName = sys.argv[1]
 
-parser.parse(fileName)
+infos, rides = parser.parse(fileName)
 
 # File name output 
 fileName = (fileName.split('.'))[0] + '.out'
