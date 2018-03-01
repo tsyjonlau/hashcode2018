1ed0b70f605b6736680e3ca413d1a98188e37138
Mina Amrouche <m.amrouche@monsieurbiz.com>
2018-03-01 20:10:00 +0100

    blabla

--- output.py
+++ output.py
@@ -2,9 +2,12 @@
 def writeoutput(fileName, results):
     file = open(fileName, 'w')
     for vehicule, rides in results.items():
-        file.write(str(vehicule))
+        file.write(str(len(rides)))
         file.write(" ")
+        first = 0
         for ride in rides:
+            if (first != 0):
+                file.write(str(" "))
             file.write(str(ride))
-            file.write(str(" "))
+            first = 1
         file.write("\n")
